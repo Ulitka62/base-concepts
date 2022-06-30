715efba6a0d9e572c35b08a269aa4832631ce025
Ulitka62 <vadborisov62@gmail.com>
2022-06-30 12:56:05 +0300

    edit the code

--- task.js
+++ task.js
@@ -23,13 +23,13 @@ function calculateTotalMortgage(percent, contribution, amount, date) {
   
   if (isNaN(percent)|| percent<0) {
     return 'Ошибка ввода данных процентной ставки'
-  }else if(isNaN(contribution)|| contribution<0){
+  } else if(isNaN(contribution)|| contribution<0){
     return 'Ошибка ввода данных суммы первоначального взноса'
-  }else if (isNaN(amount) || amount<0){
+  } else if (isNaN(amount) || amount<0){
     return 'Ошибка ввода данных суммы кредита'
   } else if (isNaN(date) || date<0){
     return 'Ошибка ввода данных даты'
-  }else {
+  } else {
     let  payMonth = S*(P+P/(((1+P)**n)-1)); // Ежемесячная оплата
     let totalAmount = (payMonth*n).toFixed(2); // Общая сумма
     console.log(totalAmount);
